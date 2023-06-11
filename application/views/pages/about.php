903439084504ad289e6322f4b879864c2e3a3818
maulana222 <ergialipfalah@gmail.com>
2023-06-11 20:18:55 +0700

    update hari keddua sebelum sidang

---
+++ application/views/pages/about.php
@@ -0,0 +1,11 @@
+<?php $this->load->view('templates/header.php'); ?>
+<?php $this->load->view('templates/navbar.php'); ?>
+
+<div class="container-fluid pt-5 gradient3" style="height: 100vh;">
+    <div class="container pt-5 f-roboto">
+        <h1 class="">Tentang Website ini </h1>
+        <p>
+            "Website Al-Quran adalah sebuah platform online yang menyediakan akses terhadap teks Al-Quran secara elektronik. Melalui website ini, pengguna dapat membaca, mencari, dan mempelajari ayat-ayat Al-Quran dalam berbagai bahasa, termasuk terjemahan, tafsir, serta fitur-fitur tambahan seperti pencarian berdasarkan kata kunci atau nomor surah. Website Al-Quran juga sering menyediakan audio atau rekaman bacaan Al-Quran untuk membantu pengguna dalam memahami dan menghafal ayat-ayat suci tersebut. Tujuan dari website Al-Quran adalah memfasilitasi akses yang mudah dan luas bagi individu yang ingin belajar dan mendalami ajaran agama Islam melalui sumber utama yang adalah Al-Quran."
+        </p>
+    </div>
+</div>
